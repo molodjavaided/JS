9ee578c056db8032f3adfe89af0c116f902bbf22
Yuri <yurchik.yuri.ru@yuandex.ru>
2025-01-29 18:01:27 +0500

    topic conditions

--- js/script.js
+++ js/script.js
@@ -1,62 +1,36 @@
-"user scrict";
+"use strict"
 
-// alert('Hello');
+// if (4 == 9) {
+//     console.log('Ol!');
+// } else {
+//     console.log('Error');
+// }
 
-// const result = confirm("Are you here&");
-// console.log(result);
+// const num = 50;
 
-// const answer = +prompt("Вам есть 18?", "18");
-// console.log(answer + 5);
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok!');
+// }
 
-// const answers = [];
+// (num === 50) ? console.log('Ok!') : console.log('Error');
 
-// answers[0] = promt('Как ваше имя?', '');
-// answers[1] = promt('Как ваша фамилия?', '');
-// answers[2] = promt('Сколько вам лет?', '');
+const num = 50;
 
-// document
-
-/* Задание на урок:
-
-1) Создать переменную numberOfFilms и в неё поместить ответ от пользователя на вопрос:
-'Сколько фильмов вы уже посмотрели?'
-
-2) Создать объект personalMovieDB и в него поместить такие свойства:
-    - count - сюда передается ответ на первый вопрос
-    - movies - в это свойство поместить пустой объект
-    - actors - тоже поместить пустой объект
-    - genres - сюда поместить пустой массив
-    - privat - в это свойство поместить boolean(логическое) значение false
-
-3) Задайте пользователю по два раза вопросы:
-    - 'Один из последних просмотренных фильмов?'
-    - 'На сколько оцените его?'
-Ответы стоит поместить в отдельные переменные
-Записать ответы в объект movies в формате:
-    movies: {
-        'logan': '8.1'
-    }
-
-Проверить, чтобы все работало без ошибок в консоли */
-
-'use strict';
-
-const numberOfFilms = +('Сколько фильмов вы уже посмотрели?', '');
-
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
-console.log(personalMovieDB);
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
