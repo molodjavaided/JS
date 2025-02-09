733138211cfa40de38f32c19306563e4ba58aa6a
Yuri <yurchik.yuri.ru@yuandex.ru>
2025-02-09 22:10:33 +0500

    JS practice function

--- js/script.js
+++ js/script.js
@@ -1,110 +1,5 @@
 'use strict';
 
-// const usdCurr = 28;
-// const discount = 0.9;
-
-// function convert(amount, curr) {
-//     return curr * amount;
-// }
-
-// function promotion(result) {
-//     console.log(result * discount);
-// }
-
-// const res = convert(500, usdCurr)
-// promotion(res);
-
-
-// function test() {
-//     for (let i = 0; i < 5; i++) {
-//         console.log(i);
-//         if (i === 3) return
-//     }
-//     console.log('Done');
-// }
-
-// test();
-
-// function doNothing() { };
-// console.log(doNothing() === undefined);
-
-// function sayHello(name) {
-//     return 'Привет ' + name
-// }
-
-// sayHello('Антон')
-
-// function returnNeighboringNumbers(num) {
-//     return [num - 1, num, num + 1];
-// }
-
-// let res = returnNeighboringNumbers(5);
-// console.log(res);
-
-// function getMathResult(base, difference) {
-//     let a = '---'
-//     for (i = 1; i <= difference; i++) {
-//         console.log((base * i));
-//     } if (typeof difference === "string" || difference <= 0) {
-//         console.log(base);
-//     }
-//     return base + a + difference
-// }
-
-// getMathResult(3, 3)
-// console.log(getMathResult(3, 3));
-
-// function getMathResult(num, times) {
-//     if (typeof (times) !== 'number' || times <= 0) {
-//         return num;
-//     }
-
-//     let str = '';
-
-//     for (let i = 1; i <= times; i++) {
-//         if (i === times) {
-//             str += `${num * i}`;
-//             // Тут без черточек в конце
-//         } else {
-//             str += `${num * i}---`;
-//             // Это тоже самое, что и
-//             // str = str + num * i + "---"
-//         }
-//     }
-
-//     return str;
-// }
-
-// getMathResult(10, 5);
-
-// const str = 'test';
-// // const arr = [1, 2, 4];
-
-// console.log(str.toUpperCase());
-// console.log(str.toLowerCase());
-
-// // console.log(arr.length);
-
-// let fruit = 'Some fruit';
-
-// console.log(fruit.indexOf('fruit'));
-
-// const logg = 'Hello world';
-
-// console.log(logg.slice(5, 11));
-
-
-// console.log(logg.substr(6, 9));
-
-// const num = 12.2;
-
-// console.log(Math.round(num));
-
-// const test = '12.2px';
-// console.log(parseInt(test));
-
-// console.log(parseFloat(test));
-
 /* Задание на урок:
 
 1) Первую часть задания повторить по уроку
@@ -184,6 +79,7 @@ function writeYourGenres() {
     for (let i = 1; i <= 3; i++) {
         const genre = prompt(`Ваш любимый жанр под номером ${i}`);
         personalMovieDB.genres[i - 1] = genre;
+        // personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`); ТОТ ЖЕ КОД ТОЛЬКО КОРОЧЕ И БЫСТРЕЕ
     }
 }
 
