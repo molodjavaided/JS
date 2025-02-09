3410f79ab94564e30d7e8b906eec41ab07c0937c
Yuri <yurchik.yuri.ru@yuandex.ru>
2025-02-09 22:09:02 +0500

    JS practice function

--- js/script.js
+++ js/script.js
@@ -1,38 +1,192 @@
-for (let i = 0; i < 3; i++) {
-    console.log(i);
-    for (let j = 0; j < 3; j++) {
-        console.log(j);
+'use strict';
+
+// const usdCurr = 28;
+// const discount = 0.9;
+
+// function convert(amount, curr) {
+//     return curr * amount;
+// }
+
+// function promotion(result) {
+//     console.log(result * discount);
+// }
+
+// const res = convert(500, usdCurr)
+// promotion(res);
+
+
+// function test() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log(i);
+//         if (i === 3) return
+//     }
+//     console.log('Done');
+// }
+
+// test();
+
+// function doNothing() { };
+// console.log(doNothing() === undefined);
+
+// function sayHello(name) {
+//     return 'Привет ' + name
+// }
+
+// sayHello('Антон')
+
+// function returnNeighboringNumbers(num) {
+//     return [num - 1, num, num + 1];
+// }
+
+// let res = returnNeighboringNumbers(5);
+// console.log(res);
+
+// function getMathResult(base, difference) {
+//     let a = '---'
+//     for (i = 1; i <= difference; i++) {
+//         console.log((base * i));
+//     } if (typeof difference === "string" || difference <= 0) {
+//         console.log(base);
+//     }
+//     return base + a + difference
+// }
+
+// getMathResult(3, 3)
+// console.log(getMathResult(3, 3));
+
+// function getMathResult(num, times) {
+//     if (typeof (times) !== 'number' || times <= 0) {
+//         return num;
+//     }
+
+//     let str = '';
+
+//     for (let i = 1; i <= times; i++) {
+//         if (i === times) {
+//             str += `${num * i}`;
+//             // Тут без черточек в конце
+//         } else {
+//             str += `${num * i}---`;
+//             // Это тоже самое, что и
+//             // str = str + num * i + "---"
+//         }
+//     }
+
+//     return str;
+// }
+
+// getMathResult(10, 5);
+
+// const str = 'test';
+// // const arr = [1, 2, 4];
+
+// console.log(str.toUpperCase());
+// console.log(str.toLowerCase());
+
+// // console.log(arr.length);
+
+// let fruit = 'Some fruit';
+
+// console.log(fruit.indexOf('fruit'));
+
+// const logg = 'Hello world';
+
+// console.log(logg.slice(5, 11));
+
+
+// console.log(logg.substr(6, 9));
+
+// const num = 12.2;
+
+// console.log(Math.round(num));
+
+// const test = '12.2px';
+// console.log(parseInt(test));
+
+// console.log(parseFloat(test));
+
+/* Задание на урок:
+
+1) Первую часть задания повторить по уроку
+
+2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
+false - выводит в консоль главный объект программы
+
+3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос
+"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
+genres
+
+P.S. Функции вызывать не обязательно*/
+
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уде посмотрели?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уде посмотрели?', '');
     }
 }
 
-// *
-// **
-// ***
-// ****
-// *****
-// ******
+// start();
 
-let result = '';
-const length = 9;
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-for (let i = 1; i < length; i++) {
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++) {
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+            b = prompt('На сколько оцените его?', '');
 
-    for (let j = 0; j < i; j++) {
-        result += "*";
-    }
-
-    result += '\n';
-}
-
-console.log(result);
-
-first: for (let i = 0; i < 3; i++) {
-    console.log(`First level: ${i}`);
-    for (let j = 0; j < 3; j++) {
-        console.log(`Second level: ${j}`);
-        for (let k = 0; k < 3; k++) {
-            if (k === 2) continue first;
-            console.log(`First level: ${k}`);
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
         }
     }
 }
+
+// rememberMyFilms();
+
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
+
+// detectPersonalLevel();
+
+
+
+function showMyDB(hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+function writeYourGenres() {
+    for (let i = 1; i <= 3; i++) {
+        const genre = prompt(`Ваш любимый жанр под номером ${i}`);
+        personalMovieDB.genres[i - 1] = genre;
+    }
+}
+
+writeYourGenres();
+
+// Код возьмите из предыдущего домашнего задания
